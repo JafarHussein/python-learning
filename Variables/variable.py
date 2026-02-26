0e00127565d7bc14229961efc014875c37925c5f
loopystack19 <sanjaygreg19@gmail.com>
2026-02-26 23:34:20 +0300

    python variables should be named following a particular set of rules, its best practise to name py variables using the snake_case naming convention

---
+++ Variables/variable.py
@@ -0,0 +1,9 @@
+name='Jafar Maalim Hussein'
+print(name)
+#Rules to be followed when naming python variables
+# 1.Case Sensitivity->Variable names in py are case sensitive
+# 2.Start Character -> A variable name must start with a letter or an underscore
+# 3. Variabe names can contain letters, digits and underscores, no spaces or special characters
+# 4. you can not use the reserved words as a variable name
+# NB:use the snake_case naming convention e.g user_name
+
