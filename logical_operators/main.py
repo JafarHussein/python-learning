5a4d79789807db82d08b5fbc948e825c87071b47
loopystack19 <sanjaygreg19@gmail.com>
2026-03-08 14:10:11 +0300

    logical operators

---
+++ logical_operators/main.py
@@ -0,0 +1,39 @@
+# Logical operators = they are used in conditional statements to:
+#                    AND=checks two or more conditions , all must be true
+#                    OR= checks if at least one condition is True
+#                    NOT= True if condition is False, False if condition is True
+
+
+
+# A sample programme for the AND operator
+
+# Write a programme that checks if a username is provided and the age provided should be equal or greater than 18
+
+# name=str(input("What is your name: "))
+# age=int(input("How old are you?: "))
+
+# if(len(name)>=8 and age>=18):
+#     print("Access granted ✅")
+# else:
+#     print("Access denied ❌")
+
+# A sample programme for the or operator
+
+# name=str(input("What is your name: "))
+# age=int(input("How old are you?: "))
+
+# if(len(name)>=8 and age>=7):
+#     print("Access granted ✅")
+# else:
+#     print("Access denied ❌")
+
+
+# A sample programme for not operator
+
+# While not found:
+      #keep searching
+
+
+
+
+
