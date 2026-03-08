a15a1aa8ea75758f246f45c91c322ba8699443ce
loopystack19 <sanjaygreg19@gmail.com>
2026-03-08 12:28:01 +0300

    math module in py

---
+++ math/main.py
@@ -0,0 +1,24 @@
+# Built in math functions
+import math
+x=3.134
+y=4
+z=5
+#Built in math functions
+
+#round()->This rounds a number to the nearest whole number
+#abs()->Returns the absolute value of a variable
+#pow(x,y)-> X is the base and Y is the exponenet
+#max()->Returns the maximum values in a provided set
+#min()->Returns the minimum values in a provided set
+
+#Available after importing math module
+
+#math.pi->Returns the value of pi
+#math.e->Returns the exponential constant
+#math.sqrt->This provides the sqrt of a variable
+#math.ceil->This rounds up a floating number
+#math.floor->This is always round
+
+
+
+
