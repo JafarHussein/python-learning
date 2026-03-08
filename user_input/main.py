08e2f41bdd10a589bbe567d54ee2611d1df37e43
loopystack19 <sanjaygreg19@gmail.com>
2026-03-08 12:20:56 +0300

    accepting user input

---
+++ user_input/main.py
@@ -0,0 +1,5 @@
+# Write a simple programme that accepts user input and calculate the area of a circle
+radius=float(input("Input the radius of your circle: "))
+PI=3.1415
+circumfrence=PI*radius*radius
+print(circumfrence)
