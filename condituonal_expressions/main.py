301fbbda453bb7a0628d1d5da4fe1de1f6f75a10
loopystack19 <sanjaygreg19@gmail.com>
2026-03-09 13:57:40 +0300

    conditional expressions in py

---
+++ condituonal_expressions/main.py
@@ -0,0 +1,16 @@
+# Conditional statements are a short cut to the if, else conditional clause
+
+# Sample programme to check if a number is even or odd
+
+import random
+
+random_num=random.randint(0, 100)
+
+results="Even Number" if random_num % 2 == 0 else "Odd Number"
+
+print(random_num)
+
+print(results)
+
+
+
